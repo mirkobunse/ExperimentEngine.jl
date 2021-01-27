73afcdf89dfaac4428f3c7b3b0f06bf70b05afc8
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2021-01-27 22:42:29 +0100

    Added a license

--- src/ExperimentEngine.jl
+++ src/ExperimentEngine.jl
@@ -1,3 +1,24 @@
+#
+# ExperimentEngine.jl
+# Copyright 2021 Mirko Bunse
+#
+#
+# Conduct experiments in parallel.
+#
+#
+# ExperimentEngine.jl is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ExperimentEngine.jl.  If not, see <http://www.gnu.org/licenses/>.
+#
 module ExperimentEngine
 
 using Distributed, ProgressMeter
